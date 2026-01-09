a7f299a717faf4815235fd998a88038db47dc70d
Hiroto SHIKADA <bambi0905hs@ezweb.ne.jp>
2026-01-09 17:09:08 +0900

    webページの更新

--- public/ide/js/shica-lib.js
+++ public/ide/js/shica-lib.js
@@ -4,17 +4,12 @@
 */
 
 mergeInto(LibraryManager.library, {
-  //WebRTC broadcast Event Object
+
+    //WebRTC broadcast Event Object
   _web_rtc_broadcast_eo_: function (index, channelPtr, passwordPtr, ptr) {
     const channel = UTF8ToString(channelPtr);
     const password = UTF8ToString(passwordPtr);
-    console.log(
-      "WebRTC Broadcast Event Object:",
-      index,
-      channel,
-      password,
-      ptr
-    );
+    console.log("WebRTC Broadcast Event Object:", index, channel, password, ptr);
     _addWebRtcBroadcast(index, channel, password, ptr);
     return 0;
   },
